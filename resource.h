68a5c33cc9bb1321152b43d7c03659f18f0ec222
Den <dsurya415@gmail.com>
2024-02-28 15:46:39 +0700

    test2

---
+++ resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MathHead.rc
+//
+#define ID_INSERT_AREAOFATRIANGLE       0
+#define IDC_MYICON                      2
+#define ID_MENU                         2
+#define IDD_MATHHEAD_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define ID_ABOUT                        104
+#define IDD_DIALOGBAR                   104
+#define ID_EXIT                         105
+#define IDI_MATHHEAD                    107
+#define IDI_SMALL                       108
+#define IDC_EXPR_TEXT                   109
+#define IDR_MAINFRAME                   128
+#define ID_EDIT                         136
+#define ID_INSERT_DISTANCEFORMULA       137
+#define ID_INSERT_VOLUMEOFASPHERE       138
+#define IDC_PARSE                       1008
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        139
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
